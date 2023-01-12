e76ca00f6ddf47c052c66717496f633ba09ab3bf
matheus.silva <tecmatheusg@hotmail.com>
2023-01-12 09:48:21 -0300

    Primeiro commit

---
+++ Parse e TryParse CSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Parse_e_TryParse_CSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool funcionou = int.TryParse("544", out int valor);
+
+            Console.WriteLine(funcionou);
+            Console.WriteLine(valor);
+            //resultado = true, 544
+
+            bool funcionou2 = int.TryParse("chute", out int valor2);
+            Console.WriteLine(funcionou2);
+            Console.WriteLine(valor2);
+            //resultado = false, 0
+
+        }
+    }
+}
